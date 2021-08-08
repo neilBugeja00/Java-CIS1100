0ac757a6727e3ab2bf0d2f72209b3dbf7d6c762e
neilBugeja00 <neilbugeja2000@gmail.com>
2021-08-08 06:21:28 +0200

    added requirements to employee & partTimer

--- AssignmentSeptember/dataobjects/Employee.java
+++ AssignmentSeptember/dataobjects/Employee.java
@@ -2,6 +2,14 @@ package dataobjects;
 import linearnodes.*;
 import linearstructures.*;
 
-public class Employee {
-    
+public class Employee extends AnyClass{
+    String surname;
+    double pay;
+
+    public Employee (int iNo, String surname, double pay) {
+        
+        super (iNo);
+        this.surname = surname;
+        this.pay = pay;        
+    }
 }
--- AssignmentSeptember/dataobjects/PartTimer.java
+++ AssignmentSeptember/dataobjects/PartTimer.java
@@ -2,6 +2,13 @@ package dataobjects;
 import linearnodes.*;
 import linearstructures.*;
 
-public class PartTimer {
-    
+public class PartTimer extends Employee{
+
+    int hours;
+
+    public PartTimer (int iNo, String surname, double pay, int hours) {
+        
+        super (iNo, surname, pay);
+        this.hours = hours;      
+    }
 }
